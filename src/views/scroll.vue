455cb1f4ee4a7bee808c8bca5d956ff7e0f689d6
lvluwebjs <564297304@qq.com>
2021-10-03 18:42:35 +0800

    scroll

---
+++ src/views/scroll.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      我是滚动页面
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style  scoped>
+</style>
